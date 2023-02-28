193ea1a02aaa2a4c35122f2c6fdb40b0344a23cd
kraigsheetz <122825959+kraigsheetz@users.noreply.github.com>
2023-02-28 13:01:07 -0500

    Basic setup with stock map zoomed on Pittsburgh

--- FinalProject/app.R
+++ FinalProject/app.R
@@ -14,43 +14,66 @@
 # Test 2
 
 library(shiny)
+library(shiny)
+library(shinythemes)
+library(leaflet)
+library(leaflet.extras)
+library(sf)
+library(shinyjs)
+library(dplyr)
+
+# Pittsburgh Playing Fields
+pittsburgh.fields.load <- st_read("https://data.wprdc.org/dataset/87c77ec3-db98-4b2a-8891-d9b577b4c44d/resource/d569b513-44c0-4b65-9241-cc3d5c506760/download/fields_img.geojson")
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+# Define UI for application
+ui <- navbarPage("NYC Green Infrastructure",
+                 theme = shinytheme("united"),
+                 tabPanel("Graphs",
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Graph inputs
+                            ),
+                            mainPanel(
+                              # Graph outputs
+                            )
+                          )
+                 ),
+                 tabPanel("Map",
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Map inputs
+                            ),
+                            mainPanel(
+                              # Using Shiny JS
+                              shinyjs::useShinyjs(),
+                              # Style the background and change the page
+                              tags$style(type = "text/css", ".leaflet {height: calc(100vh - 90px) !important;}
+                                         body {background-color: #D4EFDF;}"),
+                              # Map Output
+                              leafletOutput("leaflet")
+                            )
+                          )
+                 ),
+                 # Data Table Pannel
+                 tabPanel("Data",
+                          fluidPage(
+                            #wellPanel(DT::dataTableOutput("table"))
+                          )
+                 )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$leaflet <- renderLeaflet({
+    leaflet() %>%
+      addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", attribution = "Google", group = "Google") %>%
+      addProviderTiles(provider = providers$Wikimedia, group = "Wiki") %>%
+      setView(-79.98093, 40.444217, 12) %>%
+      addLayersControl(baseGroups = c("Google", "Wiki"))
+  })
 }
 
 # Run the application 
