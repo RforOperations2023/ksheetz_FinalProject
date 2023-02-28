4c5e90e7f60fe7b8126211a099a4d4c09bf4721e
kraigsheetz <122825959+kraigsheetz@users.noreply.github.com>
2023-02-28 15:06:10 -0500

    Loaded all three datasets - pittsburgh neighborhoods (to filter on), field data (may delete later, but using for now), and pittsburgh playgrounds (our main focus). Right now, field display is working, but when unselected app is crashing. Will fix in next commit.

--- FinalProject/app.R
+++ FinalProject/app.R
@@ -10,8 +10,7 @@
 # Kraig Sheetz
 # ksheetz
 
-# Final R Project
-# Test 2
+# Final R Project : Pittsburgh Playgrounds and Fields
 
 library(shiny)
 library(shiny)
@@ -24,26 +23,30 @@ library(dplyr)
 
 # Pittsburgh Playing Fields
 pittsburgh.fields.load <- st_read("https://data.wprdc.org/dataset/87c77ec3-db98-4b2a-8891-d9b577b4c44d/resource/d569b513-44c0-4b65-9241-cc3d5c506760/download/fields_img.geojson")
-
+#View(pittsburgh.fields.load)
+# Pittsbugh Playground Equipment
+pittsburgh.playground.load <- st_read("https://data.wprdc.org/dataset/4b9c5947-5645-4dbc-a7b6-eb323418fe02/resource/eb0cf52f-3da1-4b77-8f6d-891dff8adfa0/download/playgroundequipment_img.geojson")
+#View(pittsburgh.playground.load)
+pittsburgh.neighborhoods.load <- st_read("https://data.wprdc.org/dataset/e672f13d-71c4-4a66-8f38-710e75ed80a4/resource/4af8e160-57e9-4ebf-a501-76ca1b42fc99/download/pittsburghpaneighborhoods-.geojson")
+View(pittsburgh.neighborhoods.load)
 
 # Define UI for application that draws a histogram
 # Define UI for application
-ui <- navbarPage("NYC Green Infrastructure",
+ui <- navbarPage("Pittsburgh Green Spaces",
                  theme = shinytheme("united"),
-                 tabPanel("Graphs",
-                          sidebarLayout(
-                            sidebarPanel(
-                              # Graph inputs
-                            ),
-                            mainPanel(
-                              # Graph outputs
-                            )
-                          )
-                 ),
                  tabPanel("Map",
                           sidebarLayout(
                             sidebarPanel(
-                              # Map inputs
+                              # map inputs
+                              #pittsburgh.fields.load
+                              radioButtons(inputId = "showFields",
+                                           label = "Show Fields",
+                                           choices = c("Yes" = 1, "No" = 0),
+                                           selected = "Yes"),
+                              radioButtons(inputId = "showPlaygrounds",
+                                           label = "Show Playgrounds",
+                                           choices = c("Yes", "No"),
+                                           selected = "Yes")
                             ),
                             mainPanel(
                               # Using Shiny JS
@@ -53,6 +56,19 @@ ui <- navbarPage("NYC Green Infrastructure",
                                          body {background-color: #D4EFDF;}"),
                               # Map Output
                               leafletOutput("leaflet")
+                              # Graph outputs
+                              # If this doesn't work, put on other tab
+                            )
+                          )
+                 ),
+                 tabPanel("Graphs",
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Graph inputs
+                            ),
+                            mainPanel(
+
+                              # Graph outputs
                             )
                           )
                  ),
@@ -66,6 +82,15 @@ ui <- navbarPage("NYC Green Infrastructure",
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  # Filter fields based on user input
+  fieldInputs <- reactive({
+    fields <- pittsburgh.fields.load
+    req(input$showFields)
+    if (input$showFields == 1) {
+      return(fields)
+    }
+  })
 
   output$leaflet <- renderLeaflet({
     leaflet() %>%
@@ -73,6 +98,14 @@ server <- function(input, output) {
       addProviderTiles(provider = providers$Wikimedia, group = "Wiki") %>%
       setView(-79.98093, 40.444217, 12) %>%
       addLayersControl(baseGroups = c("Google", "Wiki"))
+  })
+  
+  observe({
+    fields <- fieldInputs()
+    leafletProxy("leaflet", data = fields) %>%
+      # In this case either lines 92 or 93 will work
+      clearShapes() %>%
+      addPolygons(popup = ~paste0("<b>", park, "</b>"),  layerId = ~id, fill = FALSE, color = "green")
   })
 }
 
